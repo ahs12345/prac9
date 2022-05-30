959225e5d8170e6b725ca8140d6b3e98dd69f827
ahs12345 <75584185+ahs12345@users.noreply.github.com>
2022-05-30 21:28:00 +0930

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -8,6 +8,24 @@
 
 using namespace std;
 
+float returnSum( float int1, float int2, char oper){
+   float sum;
+   if(oper == '+')
+       sum = float(int1)+float(int2);
+        
+
+   else if(oper == '-')
+       sum = float(int1)-float(int2);
+
+
+   else if(oper =='/')
+       sum = float(int1)/float(int2);
+
+   else if(oper=='*')
+       sum = float(int1)*float(int2);
+      
+return sum;
+}
 
 int main()
 {
@@ -16,6 +34,7 @@ int main()
     queue<int> digitQueue;
     stack<char> operatorStack;
 
+    cout << "Input: ";
     while (cin >> input){
         if (isdigit(input[0])){
             int intInput = atoi(&(input[0]));
@@ -36,6 +55,7 @@ int main()
     if (digitQueue.size() - operatorStack.size() != 1){
         cout << "Error";
     }
+    /*
     else {
     while (!digitQueue.empty()) {
         cout << ' ' << digitQueue.front();
@@ -49,6 +69,59 @@ int main()
         operatorStack.pop();
         }
     
+    }
+    */
+
+   float sum = 0;
+   if (operatorStack.size() > 1) {
+        for (int i = 0; i < operatorStack.size()-1; i++){
+            cout << '(';
+        }
+        float temp1, temp2;
+        char tempOpp;
+
+        cout << digitQueue.front() << " " << operatorStack.top() << " ";
+        temp1 = digitQueue.front();
+        tempOpp = operatorStack.top();
+
+        digitQueue.pop();
+        operatorStack.pop();
+        cout << digitQueue.front() << ")"; 
+
+        temp2 = digitQueue.front();
+        digitQueue.pop();
+        sum = returnSum(temp1, temp2, tempOpp);
+        
+
+        while (operatorStack.size() > 1){
+            cout << " " << operatorStack.top() << " " << digitQueue.front() << ")";
+            sum = returnSum(sum, digitQueue.front(), operatorStack.top());
+            operatorStack.pop();
+            digitQueue.pop();
+        }
+
+        sum = returnSum(sum, digitQueue.front(), operatorStack.top());
+        cout << " " << operatorStack.top() << " " << digitQueue.front() << " = " << sum;
+        operatorStack.pop();
+        digitQueue.pop();
+    }
+    else if(operatorStack.size() == 1){
+        float temp1, temp2;
+        char tempOpp;
+
+        cout << digitQueue.front() << " " << operatorStack.top() << " ";
+        temp1 = digitQueue.front();
+        tempOpp = operatorStack.top();
+
+        digitQueue.pop();
+        operatorStack.pop();
+        temp2 = digitQueue.front();
+        cout << digitQueue.front();
+        sum = returnSum(temp1, temp2, tempOpp);
+        cout << " = " << sum; 
+    }
+    else{
+        cout << digitQueue.front() << " = " << digitQueue.front();
     }
     return 0;
 }
